ddc34c49c74631cf6e1ef1bc9e3fce89a53ee329
Murillo Nahas <murillonahas2003@gmail.com>
2026-03-17 17:33:36 -0300

    feat: created notifications resource

---
+++ apps/api/src/modules/notifications/entities/notification.entity.ts
@@ -0,0 +1,45 @@
+import { UserEntity } from "@/modules/users/entities/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export enum NotificationStatus {
+  PENDING = 'PENDING',
+  QUEUED = 'QUEUED',
+  PROCESSING = 'PROCESSING',
+  DELIVERED = 'DELIVERED',
+  FAILED = 'FAILED',
+}
+
+export enum NotificationChannel {
+  EMAIL = 'EMAIL',
+  SMS = 'SMS',
+  PUSH = 'PUSH',
+  REALTIME = 'REALTIME',
+}
+
+@Entity('notifications')
+export class NotificationEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => UserEntity, (user) => user.notifications)
+  @JoinColumn({ name: 'user_id' })
+  user: UserEntity;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @Column({ type: 'enum', enum: NotificationChannel })
+  channel: NotificationChannel;
+
+  @Column({ type: 'text' })
+  message: string;
+
+  @Column({ type: 'enum', enum: NotificationStatus, default: NotificationStatus.PENDING })
+  status: NotificationStatus;
+
+  @UpdateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}

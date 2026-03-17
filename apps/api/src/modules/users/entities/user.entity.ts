e62daa937572765a7b787d7fba053e97c1756bab
Murillo Nahas <murillonahas2003@gmail.com>
2026-03-17 17:33:46 -0300

    feat: created users resource

---
+++ apps/api/src/modules/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { NotificationEntity } from '@/modules/notifications/entities/notification.entity';
+
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  CLIENT = 'CLIENT',
+}
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.CLIENT })
+  role: UserRole;
+
+  @Column()
+  passwordHash: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => NotificationEntity, (notification) => notification.userId)
+  notifications: NotificationEntity[];
+}

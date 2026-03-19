5bb60b12607ee5a67cce2e4e47a5870fbc121909
Murillo Nahas <murillonahas2003@gmail.com>
2026-03-18 22:44:01 -0300

    feat: created findByEmail and create methods

--- apps/api/src/modules/users/users.module.ts
+++ apps/api/src/modules/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [UsersController],
+  controllers: [],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule { }
--- apps/api/src/modules/users/users.service.ts
+++ apps/api/src/modules/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) { }
+
+  async findByEmail(email: string): Promise<UserEntity | null> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async create(data: { name: string; email: string; passwordHash: string }): Promise<UserEntity> {
+    const user = this.userRepository.create(data);
+
+    return this.userRepository.save(user);
+  }
+}

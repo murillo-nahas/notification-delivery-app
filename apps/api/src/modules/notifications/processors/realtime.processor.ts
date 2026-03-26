a9c8ff6b133904757dec55871b3c6f56301282f2
Murillo Nahas <murillonahas2003@gmail.com>
2026-03-25 21:43:58 -0300

    feat(wip): create queue processors

---
+++ apps/api/src/modules/notifications/processors/realtime.processor.ts
@@ -0,0 +1,26 @@
+import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
+import { Job } from "bullmq";
+import { NotificationEntity, NotificationStatus } from "../entities/notification.entity";
+import { NotificationsService } from "../notifications.service";
+
+@Processor('realtime')
+export class RealtimeProcessor extends WorkerHost {
+  constructor(private readonly notificationsService: NotificationsService) {
+    super();
+  }
+
+  async process(job: Job<NotificationEntity>): Promise<void> {
+    const notification = job.data;
+
+    await this.notificationsService.updateStatus(notification.id, NotificationStatus.PROCESSING);
+
+    console.log(`Sending real-time notification to ${notification.userId}`);
+
+    await this.notificationsService.updateStatus(notification.id, NotificationStatus.DELIVERED);
+  }
+
+  @OnWorkerEvent('failed')
+  onFailed(job: Job<NotificationEntity>) {
+    this.notificationsService.updateStatus(job.data.id, NotificationStatus.FAILED);
+  }
+}
---
+++ apps/api/src/modules/notifications/processors/sms.processor.ts
@@ -0,0 +1,26 @@
+import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
+import { Job } from "bullmq";
+import { NotificationEntity, NotificationStatus } from "../entities/notification.entity";
+import { NotificationsService } from "../notifications.service";
+
+@Processor('sms')
+export class SmsProcessor extends WorkerHost {
+  constructor(private readonly notificationsService: NotificationsService) {
+    super();
+  }
+
+  async process(job: Job<NotificationEntity>): Promise<void> {
+    const notification = job.data;
+
+    await this.notificationsService.updateStatus(notification.id, NotificationStatus.PROCESSING);
+
+    console.log(`Sending SMS to ${notification.userId}`);
+
+    await this.notificationsService.updateStatus(notification.id, NotificationStatus.DELIVERED);
+  }
+
+  @OnWorkerEvent('failed')
+  onFailed(job: Job<NotificationEntity>) {
+    this.notificationsService.updateStatus(job.data.id, NotificationStatus.FAILED);
+  }
+}

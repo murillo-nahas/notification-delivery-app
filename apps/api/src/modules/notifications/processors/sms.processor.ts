b65eef28d1e72cad22d8d5f68a34e739046483d8
Murillo Nahas <murillonahas2003@gmail.com>
2026-03-27 16:36:58 -0300

    feat: add sms processor

--- apps/api/src/modules/notifications/processors/sms.processor.ts
+++ apps/api/src/modules/notifications/processors/sms.processor.ts
@@ -1,12 +1,26 @@
 import { OnWorkerEvent, Processor, WorkerHost } from '@nestjs/bullmq';
-import { Job } from "bullmq";
-import { NotificationEntity, NotificationStatus } from "../entities/notification.entity";
-import { NotificationsService } from "../notifications.service";
+import { Job } from 'bullmq';
+import { NotificationEntity, NotificationStatus } from '../entities/notification.entity';
+import { NotificationsService } from '../notifications.service';
+import { Twilio } from 'twilio';
+import { UsersService } from '@/modules/users/users.service';
+import { ConfigService } from '@nestjs/config';
 
 @Processor('sms')
 export class SmsProcessor extends WorkerHost {
-  constructor(private readonly notificationsService: NotificationsService) {
+  private readonly client: Twilio;
+
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
     super();
+
+    this.client = new Twilio(
+      this.configService.get('TWILIO_ACCOUNT_SID'),
+      this.configService.get('TWILIO_AUTH_TOKEN'),
+    );
   }
 
   async process(job: Job<NotificationEntity>): Promise<void> {
@@ -14,7 +28,21 @@ export class SmsProcessor extends WorkerHost {
 
     await this.notificationsService.updateStatus(notification.id, NotificationStatus.PROCESSING);
 
-    console.log(`Sending SMS to ${notification.userId}`);
+    const user = await this.usersService.findById(notification.userId);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (!user.phoneNumber) {
+      throw new Error('User has no phone number');
+    }
+
+    await this.client.messages.create({
+      from: this.configService.get('TWILIO_PHONE_NUMBER'),
+      to: user.phoneNumber,
+      body: notification.message,
+    });
 
     await this.notificationsService.updateStatus(notification.id, NotificationStatus.DELIVERED);
   }

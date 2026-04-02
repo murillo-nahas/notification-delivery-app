bb10b53e7eaf771637c09da7c24af44e70aa36a3
Murillo Nahas <murillonahas2003@gmail.com>
2026-04-02 16:31:14 -0300

    feat: create app-layout component

---
+++ apps/admin/src/components/app-layout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router';
+import Header from './header';
+import Sidebar from './sidebar';
+
+export default function AppLayout() {
+  return (
+    <div className="flex flex-col h-screen bg-background">
+      <Header />
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 overflow-y-auto">
+          <div className="max-w-3xl mx-auto px-8 py-10">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}

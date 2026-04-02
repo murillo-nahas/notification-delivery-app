9247ce352d8d26ebf54f0b880bec5fff72b226e1
Murillo Nahas <murillonahas2003@gmail.com>
2026-04-01 21:44:02 -0300

    feat: use authprovider on main.tsx

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -4,8 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./app";
-
 import { Toaster } from "@/components/ui/sonner";
+import { AuthProvider } from "./contexts/auth-context";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,8 +19,10 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Toaster />
-      <App />
+      <AuthProvider>
+        <Toaster />
+        <App />
+      </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,

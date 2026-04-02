db1aa6a2306ba279ce342fd04d4f4fca365e4da9
Murillo Nahas <murillonahas2003@gmail.com>
2026-04-01 21:43:48 -0300

    feat: create auth context

---
+++ apps/admin/src/contexts/auth-context.tsx
@@ -0,0 +1,54 @@
+import { createContext, type ReactNode, useContext, useState } from "react";
+import { jwtDecode } from 'jwt-decode';
+
+interface AuthUser {
+  userId: string;
+  email: string;
+  role: string;
+}
+
+interface AuthContextType {
+  user: AuthUser | null;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+  const [user, setUser] = useState<AuthUser | null>(() => {
+    const token = localStorage.getItem('accessToken');
+
+    if (!token) {
+      return null;
+    }
+
+    return jwtDecode<AuthUser>(token);
+  });
+
+  const login = (token: string): void => {
+    localStorage.setItem('accessToken', token);
+
+    setUser(jwtDecode<AuthUser>(token));
+  }
+
+  const logout = (): void => {
+    localStorage.removeItem('accessToken');
+
+    setUser(null);
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>{children}</AuthContext.Provider>
+  );
+}
+
+export function useAuth(): AuthContextType {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within AuthProvider');
+  }
+
+  return context;
+}

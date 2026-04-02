df786c716aac0d888901a7cf5092745711ec0d81
Murillo Nahas <murillonahas2003@gmail.com>
2026-04-02 17:17:01 -0300

    fix: fix import

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import NotFound from "./pages/not-found";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
-import Home from "./pages/home";
 import RootRedirect from "./components/root-redirect";
 import ProtectedRoute from "./components/protected-route";
 import AppLayout from "./components/app-layout";
+import Home from "./pages/home/index";
 
 export default function App() {
   return (
